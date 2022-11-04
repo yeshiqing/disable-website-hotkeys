341b489046094cebcf937456135b389df20cf836
jeffrey <jeffreyye@qq.com>
2022-11-04 09:22:23 +0800

    tempermonkey script

---
+++ README.md
@@ -0,0 +1,6 @@
+## 仓库介绍
+本仓库中 `tempermonkey-script.js` 为 Chrome 插件 [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related) `Disable website hotkeys` 的源代码。
+
+可在 Greasy fork 中找到本脚本：https://greasyfork.org/en
+
+感谢提供思路：https://greasyfork.org/en/scripts/5819-disable-website-keyboard-hooks/code
---
+++ tempermonkey-script.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name           Disable website hotkeys
+// @description    Stop websites from hijacking keyboard shortcuts.
+// @author         yeshiqing
+// @run-at         document-start
+// @include        *
+// @grant          none
+// @version        0.0.1
+// @namespace      https://github.com/isaacl/tmscript-keycode
+// ==/UserScript==
+
+// Disable these keys when no keyboard modifier is pressed
+let std_keycodes = new Set([
+    // Add keycodes as desired
+    // 37, 38, 39, 40 // Arrow Keys.
+]);
+
+// Disable these keys when Meta key is pressed.
+let meta_keycodes = new Set([
+    // Add keycodes as desired
+    13 // Command + F
+]);
+
+// Disable these keys when Alt key is pressed.
+let alt_keycodes = new Set([
+    // Add keycodes as desired
+    // 83 // Alt + S
+]);
+
+
+let isMac = navigator.platform.indexOf('Mac') >= 0;
+
+
+document.addEventListener('keydown', function (e) {
+    let keycode_set;
+    if (isMac ? e.metaKey : e.ctrlKey) {
+        keycode_set = meta_keycodes;
+    } else if (e.altKey) {
+        keycode_set = alt_keycodes;
+    } else {
+        keycode_set = std_keycodes;
+    }
+
+    if (keycode_set.has(e.keyCode)) {
+        e.stopImmediatePropagation();
+    }
+}, true);
